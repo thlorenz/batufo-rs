fa1a3a175f98861fe5f4ae95ea62cecc1cbd9360
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-09 23:28:28 -0600

    game: fixed life time regarding arena

--- src/game.rs
+++ src/game.rs
@@ -1,25 +1,25 @@
-// use sdl2::render::TextureCreator;
-// use sdl2::video::WindowContext;
-// use crate::engine::assets::image_asset::ImageAsset;
+use crate::engine::assets::image_asset::ImageAsset;
+use sdl2::render::TextureCreator;
+use sdl2::video::WindowContext;
 
 use crate::arena::arena::Arena;
-// use crate::entities::floor_render::FloorRender;
-// use crate::game_props::TILE_SIZE;
+use crate::entities::floor_render::FloorRender;
+use crate::game_props::TILE_SIZE;
 use std::error::Error;
 
-pub struct Game {
-    arena: Arena,
-    // floor: FloorRender<'a>,
+pub struct Game<'a> {
+    arena: &'a Arena,
+    floor: FloorRender<'a>,
 }
 
-impl Game {
+impl<'a> Game<'a> {
     pub fn new(
-        arena: Arena,
-        //  texture_creator: &'static TextureCreator<WindowContext>,
-        //  floor_asset: &ImageAsset,
+        arena: &'a Arena,
+        texture_creator: &'a TextureCreator<WindowContext>,
+        floor_asset: &'a ImageAsset<'a>,
     ) -> Result<Self, Box<dyn Error>> {
-        // let floor = FloorRender::from_arena(&arena, texture_creator, floor_asset, TILE_SIZE);
+        let floor = FloorRender::from_arena(&arena, texture_creator, floor_asset, TILE_SIZE);
 
-        Ok(Game { arena })
+        Ok(Game { floor, arena })
     }
 }
