af27e30ef42a95162495a405d9c6f2b054949f83
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-10 20:17:37 -0600

    main: supporting two window settings

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,35 @@ extern crate sdl2;
 use batufo::{Config, WindowSettings};
 
 pub fn main() {
-    let window_settings = WindowSettings {
-        x: -800,
-        y: 0,
-        title: "batufo",
-        width: 800,
-        height: 800,
-        resizable: true,
-    };
+    //let window_settings = small_window();
+    let window_settings = large_window();
     let config = Config { window_settings };
     match batufo::start(&config) {
         Ok(_) => {}
         Err(err) => eprint!("fatal {:?}", err),
+    }
+}
+
+#[allow(dead_code)]
+fn large_window() -> WindowSettings {
+    WindowSettings {
+        x: -1200,
+        y: 0,
+        title: "batufo",
+        width: 1200,
+        height: 1200,
+        resizable: true,
+    }
+}
+
+#[allow(dead_code)]
+fn small_window() -> WindowSettings {
+    WindowSettings {
+        x: -600,
+        y: 0,
+        title: "batufo",
+        width: 600,
+        height: 600,
+        resizable: true,
     }
 }
